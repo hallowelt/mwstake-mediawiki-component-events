b951a0e4ba8a761a7d0a611e624bcd9df479cb9a
d.savuljesku <d.savuljesku@gmail.com>
2024-02-19 13:09:04 +0100

    Fix for interface changing upstream

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_EVENTS_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_EVENTS_VERSION', '2.0.2' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_EVENTS_VERSION', '2.0.3' );
 
 Bootstrapper::getInstance()
 	->register( 'events', function () {
--- src/BotAgent.php
+++ src/BotAgent.php
@@ -7,16 +7,18 @@ use MediaWiki\User\UserIdentity;
 class BotAgent implements UserIdentity {
 
 	/**
+	 * @param bool $wikiId
+	 *
 	 * @return int
 	 */
-	public function getId() {
+	public function getId( $wikiId = self::LOCAL ): int {
 		return 0;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function getName() {
+	public function getName(): string {
 		return "Event Bot";
 	}
 
@@ -28,18 +30,34 @@ class BotAgent implements UserIdentity {
 	}
 
 	/**
-	 * @param UserIdentity $user
+	 * @param UserIdentity|null $user
 	 *
 	 * @return false
 	 */
-	public function equals( UserIdentity $user ) {
+	public function equals( ?UserIdentity $user ): bool {
 		return false;
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function isRegistered() {
+	public function isRegistered(): bool {
 		return true;
+	}
+
+	/**
+	 * @param string $wikiId
+	 *
+	 * @return bool
+	 */
+	public function assertWiki( $wikiId ) {
+		return true;
+	}
+
+	/**
+	 * @return bool|string
+	 */
+	public function getWikiId() {
+		return self::LOCAL;
 	}
 }
