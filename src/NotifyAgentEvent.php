61a738bdb25115fea194139a618fa4a418203a14
d.savuljesku <d.savuljesku@gmail.com>
2025-05-13 13:38:17 +0200

    Add NotifyAgentEvent interface

---
+++ src/NotifyAgentEvent.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\Events;
+
+interface NotifyAgentEvent extends INotificationEvent {
+	// Events using this interface will generate a notification for the agent
+}
