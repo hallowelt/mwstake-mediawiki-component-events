45b17c3f459450d40f3475683c9730111a558c55
hslater <hamish.slater@hallowelt.com>
2025-10-30 18:48:49 +0700

    Defensive check

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -5,7 +5,7 @@ use MWStake\MediaWiki\Component\Events\Notifier;
 return [
 	'MWStake.Notifier' => static function ( \MediaWiki\MediaWikiServices $services ) {
 		$consumers = [];
-		foreach ( $GLOBALS['wgMWStakeNotificationEventConsumers'] as $consumerSpec ) {
+		foreach ( $GLOBALS['wgMWStakeNotificationEventConsumers'] ?? [] as $consumerSpec ) {
 			$consumer = $services->getObjectFactory()->createObject( $consumerSpec );
 			if ( $consumer instanceof \MWStake\MediaWiki\Component\Events\INotificationEventConsumer ) {
 				$consumers[] = $consumer;
